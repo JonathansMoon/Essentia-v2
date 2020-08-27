76446b1eaaa0794e5310db8ede7edca012558227
JonathansMoon <jonathanx56@hotmail.com>
2020-08-26 22:52:50 -0300

    add hidden attributes model

--- backend/app/Models/Patient.php
+++ backend/app/Models/Patient.php
@@ -8,4 +8,5 @@ class Patient extends Model
 {
     protected $table = 'patient';
     protected $fillable = ['name', 'email', 'gender', 'telephone', 'birthDate', 'lastAttendance'];
+    protected $hidden = ['created_at', 'updated_at'];
 }
